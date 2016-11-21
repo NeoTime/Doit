26bd6d8e8d4bd3827741710ae5a292026e853355
Jefferey Rigler <jeff@riglerdigital.com>
2016-11-20 19:59:37 -0800

    Fixed constraints on Tasks Table View on storyboard.

---
+++ CreateTaskViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CreateTaskViewController.swift
+//  Doit
+//
+//  Created by Jefferey Rigler on 11/20/16.
+//  Copyright © 2016 RiglerDigital. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+    
+    @IBOutlet weak var taskNameTextField: UITextField!
+
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    var previousVC = TasksViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func addTapped(_ sender: Any) {
+         // Create a Task from the Outlets //
+        
+        let task = Task()
+        task.name = taskNameTextField.text!
+        task.important = importantSwitch.isOn
+        
+         // Add new task to the list -previous viewController //
+        
+        previousVC.tasks.append(task)
+    }
+    
+    
+}
