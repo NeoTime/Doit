1381b245d65658f49cb36c10675dc745415ecaae
Jefferey Rigler <jeff@riglerdigital.com>
2016-11-20 16:48:04 -0800

    Updated main table view and created segues for the app.

---
+++ Doit/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  Doit
+//
+//  Created by Jefferey Rigler on 11/20/16.
+//  Copyright © 2016 RiglerDigital. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
+
+
